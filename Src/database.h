ef4ba803aa790f78778d2a206fdc9c191b64d988
Becky.Engley <Becky.Engley@7ba8ebcc-3671-11de-9210-07d028069593>
2009-05-16 23:48:20 +0000

    first commit of headerfiles

---
+++ Src/database.h
@@ -0,0 +1,20 @@
+#include "tradeSystem.h"
+
+commod comDatabase[14];
+
+comDatabase = { {"Jun_CL", LONG, 0, 10, 0.01}
+                {"May_HO", LONG, 0, 4.2, 0.0001}
+                {"Jun_LH", LONG, 0, 10, 0.025}
+                {"Jun_HU", LONG, 0, 4.2, 0.0001}
+                {"Aug_PB", SHORT, 0, 10, 0.025}
+                {"Sep_OJ", SHORT, 0, 7.5, 0.05}
+                {"Dec_C", SHORT, 0, 12.5, .25}
+                {"Dec_LH", SHORT, 0, 10, 0.025}
+                {"Dec_KW", SHORT, 0, 12.5, 0.25}
+                {"Nov_HO", LONG, 0, 4.2 0.0001}
+                {"Nov_CL", LONG, 0, 10, 0.01}
+                {"Oct_LH", LONG, 0, 10. 0.025}
+                {"Feb_CL", SHORT, 0, 10, 0.01}
+                {"Apr_LC", LONG, 0, 10, 0.025} };
+
+
