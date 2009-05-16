89e4764c45d49a9c4b96ce3592bb1f35dbe1c6d5
Becky.Engley <Becky.Engley@7ba8ebcc-3671-11de-9210-07d028069593>
2009-05-16 23:49:36 +0000

    added commodities.h

---
+++ Src/commodities.h
@@ -0,0 +1,10 @@
+/*
+ * header file commodities.h
+ *
+ * includes prototype for tradeSystem function
+ */
+
+
+double tradeSystem(char *szName, int iYear, int iEntryWindow, int iTrailStopWindow, int iStopLossWindow, 
+    char *szEntryDate, char *szNoEntryDate, char *szExitDate);
+
