bd0723428af1657f30c7f025711ea01ecf67f2e8
Marty.Neal <Marty.Neal@7ba8ebcc-3671-11de-9210-07d028069593>
2009-05-17 01:53:17 +0000

    * added a slash to the end of DATAPATH * capped the ifndef variable

--- Src/tradeSystem.h
+++ Src/tradeSystem.h
@@ -8,36 +8,28 @@
  *
  */
 
-#ifndef __trade_system__
-#define __trade_system__
+#ifndef __TRADE_SYSTEM__
+#define __TRADE_SYSTEM__
 
-
-#define DATAPATH "../Data"
+#define DATAPATH "../Data/"
 #define SHORT 0
 #define LONG 1
 
 
 typedef struct commodity
-{   
+{
     char* szName;
-    int iType;         
+    int iType;
     int iYear;
     double dTickVal;
     double dTickSize;
 } commod;
 
-int generateTradeData(commod comCommodity, double** padLow, double** padHigh, double** padOpen, 
+int generateTradeData(commod comCommodity, double** padLow, double** padHigh, double** padOpen,
     double** padClose, char*** paszDates, int* piSize);
 
-int generateChannels(commod comCommodity, double* adLow, double* adHigh, int iEntryWindow, 
+int generateChannels(commod comCommodity, double* adLow, double* adHigh, int iEntryWindow,
     int iTrailStopWindow, int iStopLossWindow, double** padEntryChannel, double** padTrailStopChannel,
     double** padStopLossChannel);
 
-
-
-#endif 
-
-
-
-
-
+#endif
