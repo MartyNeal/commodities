88df032a4603e5c0ecf84da773d64059ed0b3663
Marty.Neal <Marty.Neal@7ba8ebcc-3671-11de-9210-07d028069593>
2009-05-25 05:49:09 +0000

    lots of changes.  simulated annealing mostly works

--- trunk/Src/commodities.h
+++ trunk/Src/commodities.h
@@ -7,9 +7,16 @@
  * May 2009
  */
 
+#define UNIFORM 1
+#define NORMAL 2
+#define CONSTANT 3
 
 double tradeSystem(char* szName, int iYear, int iEntryWindow, int iTrailStopWindow, int iStopLossWindow,
                    char* szEntryDate, char* szNoEntryDate, char* szExitDate);
 
 double tradeSystemData(char* szName, double dPercentData, int iEntryWindow, int iTrailStopWindow,
                        int iStopLossWindow, char* szEntryDateMonthDay, char* szNoEntryDateMonthDay, char* szExitDateMonthDay);
+
+double SimulatedAnnealing(int iIterations, char* szCommodName, int iArgsToRandomize, int distType);
+
+double RandomAlgorithm(int iIterations, char* szCommodName, int iArgsToRandomize, int distType);
--- trunk/Src/tradeSystem.h
+++ trunk/Src/tradeSystem.h
@@ -20,6 +20,15 @@
 #define ERRVAL -1
 #define TRAIN_YEARS 2
 #define TEST_YEARS 3
+#define TRAIN_YEARS_PERCENTAGE .67
+#define TEST_YEARS_PERCENTAGE (1 - TRAIN_YEARS_PERCENTAGE)
+#define MAX_WINDOW_SIZE 50
+#define MAX_DATE_SIZE 365
+#define STDDEV 4 // 1/4th
+
+#define UNIFORM 1
+#define NORMAL 2
+#define CONSTANT 3
 
 typedef struct commodity
 {
@@ -31,11 +40,25 @@ typedef struct commodity
     double dTickSize;
 } commod;
 
+double tradeSystem(char* szName, int iYear, int iEntryWindow, int iTrailStopWindow, int iStopLossWindow,
+                   char* szEntryDate, char* szNoEntryDate, char* szExitDate);
+
+double tradeSystemData(char* szName, double dPercentData, int iEntryWindow, int iTrailStopWindow,
+                       int iStopLossWindow, char* szEntryDateMonthDay, char* szNoEntryDateMonthDay, char* szExitDateMonthDay);
+
+void Neighbor(int* piCurEntryWindow, int* piCurTrailStopWindow,
+              int* piCurStopLossWindow, char* szCurEntryDate,
+              char* szCurNoEntryDate, char* szCurExitDate,
+              int iArgsToRandomize, int distType);
+
 int generateTradeData(commod comCommodity, double** padLow, double** padHigh, double** padOpen,
-    double** padClose, char*** paszDates, int* piSize);
+                      double** padClose, char*** paszDates, int* piSize);
 
 int generateChannels(commod comCommodity, double* adLow, double* adHigh, int iEntryWindow,
                      int iTrailStopWindow, int iStopLossWindow, int iSize, double** padEntryChannel, double** padTrailStopChannel,
-    double** padStopLossChannel);
+                     double** padStopLossChannel);
+
+double rnorm();
+double pnorm(double x, double mu, double sigma);
 
 #endif
