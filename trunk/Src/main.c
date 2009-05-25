37fa10f81cb26bc0c9091b93932dfbce92b75e8a
Marty.Neal <Marty.Neal@7ba8ebcc-3671-11de-9210-07d028069593>
2009-05-25 16:49:19 +0000

    Removed old commented code Added ability to define an experiment and write it to a file Included MPI stuff

--- trunk/Src/main.c
+++ trunk/Src/main.c
@@ -1,82 +1,92 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include "commodities.h"
-
+#include "tradeSystem.h"
 #define NUMARGS 9
+
+//#define USE_MPI //comment out when not using MPI
+
+typedef struct _experiment {
+    double (*LearningFunction)(int iIterations, char* szCommodName, int iArgsToRandomize, int distType);
+    int distType;
+    int iArgsToChange;
+    char szName[MAX_NAME_LEN];
+    int iNumIterations;
+    int iStepSize;
+    int iAverageAccuracy;
+    char szFileName[256];
+} experiment;
 
 int main(int argc, char** argv)
 {
-    /*
-    char* szName;
-    double dPercent;
-    int iYear;
-    int iEntryWindow;
-    int iTrailStopWindow;
-    int iStopLossWindow;
-    char* szEntryDate;
-    char* szNoEntryDate;
-    char* szExitDate;
-
-    double dProfit;
-
-    if(argc != NUMARGS)
-    {
-        fprintf(stderr, "Usage: %s <commodity name> <year | percent> <entry window> <trail stop window> "
-            "<stop loss window>\n\t<entry date> <no entry date> <exit date>\n", argv[0]);
-        exit(1);
-    }
-
-    szName = (char*) argv[1];
-    dPercent = atof(argv[2]);
-    iYear = atoi(argv[2]);
-    iEntryWindow = atoi(argv[3]);
-    iTrailStopWindow = atoi(argv[4]);
-    iStopLossWindow = atoi(argv[5]);
-    szEntryDate = (char*) argv[6];
-    szNoEntryDate = (char*) argv[7];
-    szExitDate = (char*) argv[8];
-
-    if(dPercent <= 1 && dPercent >= 0) //assume percentage
-    {
-        printf("Percent value in correct range, calling tradeSystemData -----------\n");
-        dProfit = tradeSystemData(szName, dPercent, iEntryWindow, iTrailStopWindow, iStopLossWindow,
-            szEntryDate, szNoEntryDate, szExitDate);
-    }
-    else
-    {
-        printf("Calling tradeSystem -----------------------------------------------\n");
-        dProfit = tradeSystem(szName, iYear, iEntryWindow, iTrailStopWindow, iStopLossWindow,
-            szEntryDate, szNoEntryDate, szExitDate);
-    }
-
-    printf("in main.c ------------------------\n");
-
-    printf("commodity name: %s\nyear: %d\nentry window: %d\n", szName, iYear, iEntryWindow);
-    printf("trail stop window: %d\nstop loss window: %d\n", iTrailStopWindow, iStopLossWindow);
-    printf("entry date: %s\nno entry date: %s\nexit date: %s\n", szEntryDate, szNoEntryDate, szExitDate);
-    printf("profit earned: %f\n", dProfit);
-    */
-
     int i;
-    int sumR;
-    int sumS;
     int j;
-    int iIterations = 200;
+    int sum;
     time_t tTime;
+    int fd;
+    int iMyRank = 0;
+    char szWriteBuf[256] = {0};
+
+#ifdef USE_MPI
+#include <mpi.h>
+    int ipes = 0;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &ipes);
+    MPI_Comm_rank(MPI_COMM_WORLD, &iMyRank);
+#endif
+
     srand(time(&tTime));
 
-    printf("i\tRandom\tSimulated\n");
-    for(i = 0; i < iIterations; i += 10)
+    experiment aeExperiments[] = {
+        {&SimulatedAnnealing,UNIFORM,1,"Jun_CL",20,10,100,"SimulatedAnnealing_UNIFORM_1_Jun_CL_200_10_100.dat"},
+        {&SimulatedAnnealing,UNIFORM,3,"Jun_CL",200,10,100,"SimulatedAnnealing_UNIFORM_3_Jun_CL_200_10_100.dat"},
+        {&SimulatedAnnealing,UNIFORM,6,"Jun_CL",200,10,100,"SimulatedAnnealing_UNIFORM_6_Jun_CL_200_10_100.dat"},
+        {&SimulatedAnnealing,NORMAL,1,"Jun_CL",200,10,100,"SimulatedAnnealing_NORMAL_1_Jun_CL_200_10_100.dat"},
+        {&SimulatedAnnealing,NORMAL,3,"Jun_CL",200,10,100,"SimulatedAnnealing_NORMAL_3_Jun_CL_200_10_100.dat"},
+        {&SimulatedAnnealing,NORMAL,6,"Jun_CL",200,10,100,"SimulatedAnnealing_NORMAL_6_Jun_CL_200_10_100.dat"},
+        {&SimulatedAnnealing,CONSTANT,1,"Jun_CL",200,10,100,"SimulatedAnnealing_CONSTANT_1_Jun_CL_200_10_100.dat"},
+        {&SimulatedAnnealing,CONSTANT,3,"Jun_CL",200,10,100,"SimulatedAnnealing_CONSTANT_3_Jun_CL_200_10_100.dat"},
+        {&SimulatedAnnealing,CONSTANT,6,"Jun_CL",200,10,100,"SimulatedAnnealing_CONSTANT_6_Jun_CL_200_10_100.dat"},
+        {&RandomAlgorithm,UNIFORM,1,"Jun_CL",200,10,100,"RandomAlgorithm_UNIFORM_1_Jun_CL_200_10_100.dat"},
+        {&RandomAlgorithm,UNIFORM,3,"Jun_CL",200,10,100,"RandomAlgorithm_UNIFORM_3_Jun_CL_200_10_100.dat"},
+        {&RandomAlgorithm,UNIFORM,6,"Jun_CL",200,10,100,"RandomAlgorithm_UNIFORM_6_Jun_CL_200_10_100.dat"},
+        {&RandomAlgorithm,NORMAL,1,"Jun_CL",200,10,100,"RandomAlgorithm_NORMAL_1_Jun_CL_200_10_100.dat"},
+        {&RandomAlgorithm,NORMAL,3,"Jun_CL",200,10,100,"RandomAlgorithm_NORMAL_3_Jun_CL_200_10_100.dat"},
+        {&RandomAlgorithm,NORMAL,6,"Jun_CL",200,10,100,"RandomAlgorithm_NORMAL_6_Jun_CL_200_10_100.dat"},
+        {&RandomAlgorithm,CONSTANT,1,"Jun_CL",200,10,100,"RandomAlgorithm_CONSTANT_1_Jun_CL_200_10_100.dat"},
+        {&RandomAlgorithm,CONSTANT,3,"Jun_CL",200,10,100,"RandomAlgorithm_CONSTANT_3_Jun_CL_200_10_100.dat"},
+        {&RandomAlgorithm,CONSTANT,6,"Jun_CL",200,10,100,"RandomAlgorithm_CONSTANT_6_Jun_CL_200_10_100.dat"}
+    };
+
+    experiment* peMyExpirment = &aeExperiments[iMyRank];
+
+    fd = creat(peMyExpirment->szFileName,0644);
+    if (fd == -1)
     {
-        sumR = 0;
-        sumS = 0;
-        for(j = 0; j < 100; j++)
-        {
-            sumR += RandomAlgorithm(i,"Jun_CL",6,UNIFORM);
-            sumS += SimulatedAnnealing(i,"Jun_CL",6,NORMAL);
-        }
-        printf("%d\t%d\t%d\n",i,sumR/j,sumS/j);
+        fprintf(stderr,"failed to open %s\n",peMyExpirment->szFileName);
+        return -1;
     }
+
+    j = strlen(strcpy(szWriteBuf,"i\tprofit\n"));
+    write(fd,szWriteBuf,j);
+    for(i = 0; i <= peMyExpirment->iNumIterations; i += peMyExpirment->iStepSize)
+    {
+        sum = 0;
+        for(j = 0; j < peMyExpirment->iAverageAccuracy; j++)
+        {
+            sum += peMyExpirment->LearningFunction(i,peMyExpirment->szName,
+                                                    peMyExpirment->iArgsToChange,
+                                                    peMyExpirment->distType);
+        }
+        j = snprintf(szWriteBuf,256,"%d\t%d\n",i,sum/j);
+        write(fd,szWriteBuf,j);
+    }
+    close (fd);
     return 0;
 }
