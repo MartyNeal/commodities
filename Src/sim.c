493cac88d9286b51fa0cb91b4edfac4d28294127
Marty.Neal <Marty.Neal@7ba8ebcc-3671-11de-9210-07d028069593>
2009-05-27 21:09:54 +0000

    Moved non-simulated annealing specific utility functions to a seperate learner.c file.

--- Src/sim.c
+++ Src/sim.c
@@ -13,11 +13,6 @@ double StayHot(int iCurrentIteration, int iTotalIterations);
 
 double LinearCoolingFunction(int iCurrentIteration, int iTotalIterations);
 
-int Randomize(int* piCur, int iMin, int iMax, int distType);
-
-int DateToDays(char* szDate);
-
-void DaysToDate(char* szDate, int iDay);
 
 /* ----------------------------- Simulated Annealing ------------------------ */
 
@@ -167,95 +162,4 @@ double StayHot(int iCurrentIteration, int iTotalIterations)
 double LinearCoolingFunction(int iCurrentIteration, int iTotalIterations)
 {
     return 1 - (double)iCurrentIteration/iTotalIterations;
-}
-
-
-int Randomize(int* piCur, int iMin, int iMax, int distType)
-{
-    do
-    {
-        switch(distType)
-        {
-        case UNIFORM:
-            *piCur = (int)(random()%(iMax-iMin)+iMin); //equal chance of jumping anywhere
-            break;
-        case NORMAL:
-            *piCur = (int)(rnorm()*iMax/STDDEV+*piCur); //an STDDEV of 90 days
-            break;
-        case CONSTANT:
-            *piCur = random()%2 > 0 ? 1 : -1 + *piCur; // move only by one
-            break;
-        default:
-            fprintf(stderr,"ERROR:  incorrect distType (%d)\n",distType);
-            return ERRVAL;
-        }
-    }
-    while (*piCur < iMin || *piCur > iMax);
-    return 0;
-}
-
-void Neighbor(int* piCurEntryWindow, int* piCurTrailStopWindow,
-              int* piCurStopLossWindow, char* szCurEntryDate,
-              char* szCurNoEntryDate, char* szCurExitDate,
-              int iArgsToRandomize, int distType)
-{
-    int i;
-    int* piTemp;
-    int r;
-    int piCurEntryDate[] = {DateToDays(szCurEntryDate)};
-    int piCurNoEntryDate[] = {DateToDays(szCurNoEntryDate)};
-    int piCurExitDate[] = {DateToDays(szCurExitDate)};
-    int* ppiArgs[] = {
-        piCurEntryWindow, piCurTrailStopWindow, piCurStopLossWindow,
-        piCurEntryDate,   piCurNoEntryDate,     piCurExitDate};
-    int iMax;
-
-    for (i = 0; i < iArgsToRandomize; i++)
-    {
-        r = (int)random()%(6-i)+i;
-        piTemp = ppiArgs[r];
-        ppiArgs[r] = ppiArgs[i];
-        ppiArgs[i] = piTemp;
-        if (ppiArgs[i] == piCurEntryWindow ||
-            ppiArgs[i] == piCurTrailStopWindow ||
-            ppiArgs[i] == piCurStopLossWindow)
-            iMax = MAX_WINDOW_SIZE;
-        else
-            iMax = MAX_DATE_SIZE;
-
-        Randomize(ppiArgs[i],1,iMax,distType);
-    }
-
-    DaysToDate(szCurEntryDate,*piCurEntryDate);
-    DaysToDate(szCurNoEntryDate,*piCurNoEntryDate);
-    DaysToDate(szCurExitDate,*piCurExitDate);
-}
-
-const int iDaysInMonths[] = {31,28,31,30,31,30,31,31,30,31,30,31};
-
-int DateToDays(char* szDate)
-{
-    int iDate = atoi(szDate);
-    int iDay = iDate % 100;
-    int iMonth = iDate/100 - 1;
-
-    while (iMonth > 0)
-    {
-        iDay += iDaysInMonths[iMonth--];
-    }
-
-    return iDay;
-}
-
-
-void DaysToDate(char* szDate, int iDay)
-{
-    int iMonth = 0;
-
-    while (iDay > iDaysInMonths[iMonth])
-    {
-        iDay -= iDaysInMonths[iMonth++];
-    }
-
-    sprintf(szDate,"%.2d%.2d",++iMonth,iDay);
 }
