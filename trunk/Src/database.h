b3ccb9ad621f01a359dd58b5cbf0f5b8a2df9b47
Marty.Neal <Marty.Neal@7ba8ebcc-3671-11de-9210-07d028069593>
2009-05-29 20:21:39 +0000

    Sep_OJ goes back to 68 not 86

--- trunk/Src/database.h
+++ trunk/Src/database.h
@@ -25,7 +25,7 @@ commod comDatabase[NUMCOMMODS] = {  {"Jun_CL", LONG, 1983, 21, 10, 0.01},
                                     {"Jun_LH", LONG, 1967, 38, 10, 0.025},
                                     {"Jun_HU", LONG, 1985, 20, 4.2, 0.0001},
                                     {"Aug_PB", SHORT, 1962, 43, 10, 0.025},
-                                    {"Sep_OJ", SHORT, 1986, 37, 7.5, 0.05},
+                                    {"Sep_OJ", SHORT, 1968, 37, 7.5, 0.05},
                                     {"Dec_C", SHORT, 1949, 56, 12.5, .25},
                                     {"Dec_LH", SHORT, 1966, 39, 10, 0.025},
                                     {"Dec_KW", SHORT, 1966, 39, 12.5, 0.25},
@@ -34,5 +34,3 @@ commod comDatabase[NUMCOMMODS] = {  {"Jun_CL", LONG, 1983, 21, 10, 0.01},
                                     {"Oct_LH", LONG, 1966, 39, 10, 0.025},
                                     {"Feb_CL", SHORT, 1984, 21, 10, 0.01},
                                     {"Apr_LC", LONG, 1965, 40, 10, 0.025} };
-
-
