8cb66e1b425d1d19420e60525ef5334fb0377739
Marty.Neal <Marty.Neal@7ba8ebcc-3671-11de-9210-07d028069593>
2009-05-30 20:51:14 +0000

    Inserted doxygen comments lots of formatting changes made 80-char columns removed old commented out code removed unnecessary #includes changed fprintf's to FPRINTE macro moved intermediate iteration instrumentation into the learner functions changed ga's mutate function to use distType instead of hardcoded UNIFORM Fixed Quicksort bug to include a base case.  OOPS removed unused NUMARGS define added genetic algorithms to list of experiments shortened filenames that hold the experiment results pulled iIterations, iStepSize, and iAverageAccuracy out of the experiment struct and put them into defines created a node struct to hold a set of 6 params.  also typedefed indivudal to the same struct fixed mem leak in tradeSystem fixed insignificant typecast error by returning 1.0 on ERRVAL in tradeSystem changed MAX_NAME_LEN to 7 from 6 to include room for a null char

--- trunk/Src/commodities.h
+++ trunk/Src/commodities.h
@@ -1,22 +1,29 @@
-/*
- * header file commodities.h
- *
- * includes prototype for tradeSystem function
- *
- * Becky Engley and Martin Neal
- * May 2009
- */
+/*!
+  \file    commodities.h
+  \brief   Includes prototype for tradeSystem function
+  \author  Becky Engley and Martin Neal
+  \date    May 2009
+*/
 
 #define UNIFORM 1
 #define NORMAL 2
 #define CONSTANT 3
 
-double tradeSystem(char* szName, int iYear, int iEntryWindow, int iTrailStopWindow, int iStopLossWindow,
-                   char* szEntryDate, char* szNoEntryDate, char* szExitDate);
+double tradeSystem(char* szName, int iYear, int iEntryWindow,
+                   int iTrailStopWindow, int iStopLossWindow, char* szEntryDate,
+                   char* szNoEntryDate, char* szExitDate);
 
-double tradeSystemData(char* szName, double dPercentData, int iEntryWindow, int iTrailStopWindow,
-                       int iStopLossWindow, char* szEntryDateMonthDay, char* szNoEntryDateMonthDay, char* szExitDateMonthDay);
+double tradeSystemData(char* szName, double dPercentData, int iEntryWindow,
+                       int iTrailStopWindow, int iStopLossWindow,
+                       char* szEntryDateMonthDay, char* szNoEntryDateMonthDay,
+                       char* szExitDateMonthDay);
 
-double SimulatedAnnealing(int iIterations, char* szCommodName, int iArgsToRandomize, int distType);
+int SimulatedAnnealing(int iIterations, char* szCommodName,
+                       int iArgsToRandomize, int distType, double dResults[],
+                       int iResultSize);
 
-double RandomAlgorithm(int iIterations, char* szCommodName, int iArgsToRandomize, int distType);
+int GeneticAlgorithm(int iIterations, char* szCommodName, int iArgsToRandomize,
+                     int distType, double dResults[], int iResultSize);
+
+int RandomAlgorithm(int iIterations, char* szCommodName, int iArgsToRandomize,
+                    int distType, double dResults[], int iResultSize);
