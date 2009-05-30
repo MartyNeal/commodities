b2116a7ed457738da23a3331b510d23f753696fc
Marty.Neal <Marty.Neal@7ba8ebcc-3671-11de-9210-07d028069593>
2009-05-30 20:51:30 +0000

    Inserted doxygen comments lots of formatting changes made 80-char columns removed old commented out code removed unnecessary #includes changed fprintf's to FPRINTE macro moved intermediate iteration instrumentation into the learner functions changed ga's mutate function to use distType instead of hardcoded UNIFORM Fixed Quicksort bug to include a base case.  OOPS removed unused NUMARGS define added genetic algorithms to list of experiments shortened filenames that hold the experiment results pulled iIterations, iStepSize, and iAverageAccuracy out of the experiment struct and put them into defines created a node struct to hold a set of 6 params.  also typedefed indivudal to the same struct fixed mem leak in tradeSystem fixed insignificant typecast error by returning 1.0 on ERRVAL in tradeSystem changed MAX_NAME_LEN to 7 from 6 to include room for a null char

--- trunk/Src/randomAlgorithm.c
+++ trunk/Src/randomAlgorithm.c
@@ -1,66 +1,115 @@
-#include <stdlib.h>
+/*!
+  \file    randomAlgorithm.c
+  \brief   Algorithm for learning via a random walk
+  \author  Becky Engley and Martin Neal
+  \date    May 2009
+*/
+
+#include <stdio.h>
 #include <string.h>
 #include "tradeSystem.h"
 
 
-double RandomAlgorithm(int iIterations, char* szCommodName, int iArgsToRandomize, int distType)
+int RandomAlgorithm(int iIterations, char* szCommodName, int iArgsToRandomize,
+                    int distType, double dResults[], int iResultSize)
 {
-    int iCurEntryWindow;
-    int iCurTrailStopWindow;
-    int iCurStopLossWindow;
-    char szCurEntryDate[5];
-    char szCurNoEntryDate[5];
-    char szCurExitDate[5];
-    double dCurProfit;
+    node nodeCur, nodeBest;
 
-    int iBestEntryWindow;
-    int iBestTrailStopWindow;
-    int iBestStopLossWindow;
-    char szBestEntryDate[5];
-    char szBestNoEntryDate[5];
-    char szBestExitDate[5];
-    double dBestProfit;
+    int i, iStepSize;
 
-    int i;
+    if (iResultSize == 1)
+    {
+        FPRINTE(stderr,"iResultSize can't be 1\n");
+        return ERRVAL;
+    }
+
+    if (iResultSize > iIterations + 1)
+    {
+        FPRINTE(stderr,"iResultSize can't be greater than iIterations\n");
+        return ERRVAL;
+    }
+
+    iStepSize = iIterations / (iResultSize - 1);
+    if (iIterations % (iResultSize - 1) != 0)
+    {
+        FPRINTE(stderr,"iResultSize is not compatible with iIterations\n");
+        return ERRVAL;
+    }
 
     //set current and best to all uniformly random parameters
-    Neighbor(&iCurEntryWindow, &iCurTrailStopWindow, &iCurStopLossWindow,
-             szCurEntryDate, szCurNoEntryDate, szCurExitDate, 6,
-             UNIFORM);
-    dCurProfit = tradeSystemData(szCommodName, TRAIN_YEARS,
-                                 iCurEntryWindow, iCurTrailStopWindow,
-                                 iCurStopLossWindow, szCurEntryDate,
-                                 szCurNoEntryDate, szCurExitDate);
+    Neighbor(&nodeCur.iEntryWindow,
+             &nodeCur.iTrailStopWindow,
+             &nodeCur.iStopLossWindow,
+             nodeCur.szEntryDate,
+             nodeCur.szNoEntryDate,
+             nodeCur.szExitDate,
+             6, UNIFORM);
 
-    iBestEntryWindow = iCurEntryWindow;
-    iBestTrailStopWindow = iCurTrailStopWindow;
-    iBestStopLossWindow = iCurStopLossWindow;
-    strcpy(szBestEntryDate, szCurEntryDate);
-    strcpy(szBestNoEntryDate, szCurNoEntryDate);
-    strcpy(szBestExitDate, szCurExitDate);
-    dBestProfit = dCurProfit;
+    nodeCur.dProfit = tradeSystemData(szCommodName, TRAIN_YEARS,
+                                      nodeCur.iEntryWindow,
+                                      nodeCur.iTrailStopWindow,
+                                      nodeCur.iStopLossWindow,
+                                      nodeCur.szEntryDate,
+                                      nodeCur.szNoEntryDate,
+                                      nodeCur.szExitDate);
+
+    memcpy(&nodeBest, &nodeCur, sizeof(node));
 
     for (i = 0; i < iIterations; i++)
     {
-        Neighbor(&iCurEntryWindow,&iCurTrailStopWindow,&iCurStopLossWindow,
-                 szCurEntryDate,szCurNoEntryDate,szCurExitDate,
-                 iArgsToRandomize,distType);
-        dCurProfit = tradeSystemData(szCommodName, TRAIN_YEARS,
-                                     iCurEntryWindow, iCurTrailStopWindow, iCurStopLossWindow,
-                                     szCurEntryDate, szCurNoEntryDate, szCurExitDate);
-        if (dCurProfit > dBestProfit)
+        if (i % iStepSize == 0)
         {
-            iBestEntryWindow = iCurEntryWindow;
-            iBestTrailStopWindow = iCurTrailStopWindow;
-            iBestStopLossWindow = iCurStopLossWindow;
-            strcpy(szBestEntryDate,szCurEntryDate);
-            strcpy(szBestNoEntryDate,szCurNoEntryDate);
-            strcpy(szBestExitDate,szCurExitDate);
-            dBestProfit = dCurProfit;
+            dResults[i/iStepSize] =
+                tradeSystemData(szCommodName,
+                                TEST_YEARS,
+                                nodeBest.iEntryWindow,
+                                nodeBest.iTrailStopWindow,
+                                nodeBest.iStopLossWindow,
+                                nodeBest.szEntryDate,
+                                nodeBest.szNoEntryDate,
+                                nodeBest.szExitDate);
+        }
+
+        Neighbor(&nodeCur.iEntryWindow,
+                 &nodeCur.iTrailStopWindow,
+                 &nodeCur.iStopLossWindow,
+                 nodeCur.szEntryDate,
+                 nodeCur.szNoEntryDate,
+                 nodeCur.szExitDate,
+                 6, UNIFORM);
+
+        nodeCur.dProfit = tradeSystemData(szCommodName, TRAIN_YEARS,
+                                          nodeCur.iEntryWindow,
+                                          nodeCur.iTrailStopWindow,
+                                          nodeCur.iStopLossWindow,
+                                          nodeCur.szEntryDate,
+                                          nodeCur.szNoEntryDate,
+                                          nodeCur.szExitDate);
+        //if its the best so far, save it
+        if (nodeCur.dProfit > nodeBest.dProfit)
+        {
+            memcpy(&nodeBest,&nodeCur,sizeof(node));
         }
     }
-    dCurProfit = tradeSystemData(szCommodName,TEST_YEARS,
-                                 iCurEntryWindow, iCurTrailStopWindow, iCurStopLossWindow,
-                                 szCurEntryDate, szCurNoEntryDate, szCurExitDate);
-    return dCurProfit;
+
+    dResults[i/iStepSize] =
+        tradeSystemData(szCommodName,
+                        TEST_YEARS,
+                        nodeBest.iEntryWindow,
+                        nodeBest.iTrailStopWindow,
+                        nodeBest.iStopLossWindow,
+                        nodeBest.szEntryDate,
+                        nodeBest.szNoEntryDate,
+                        nodeBest.szExitDate);
+
+    printf("%d\t%d\t%d\t%s\t%s\t%s\t%.2lf\n",
+           nodeBest.iEntryWindow,
+           nodeBest.iTrailStopWindow,
+           nodeBest.iStopLossWindow,
+           nodeBest.szEntryDate,
+           nodeBest.szNoEntryDate,
+           nodeBest.szExitDate,
+           dResults[i/iStepSize]);
+
+    return 0;
 }
