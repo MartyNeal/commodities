5496157e5a2987cf7a238886ff4e1e1d330ffe3d
Marty.Neal <Marty.Neal@7ba8ebcc-3671-11de-9210-07d028069593>
2009-05-30 20:51:16 +0000

    Inserted doxygen comments lots of formatting changes made 80-char columns removed old commented out code removed unnecessary #includes changed fprintf's to FPRINTE macro moved intermediate iteration instrumentation into the learner functions changed ga's mutate function to use distType instead of hardcoded UNIFORM Fixed Quicksort bug to include a base case.  OOPS removed unused NUMARGS define added genetic algorithms to list of experiments shortened filenames that hold the experiment results pulled iIterations, iStepSize, and iAverageAccuracy out of the experiment struct and put them into defines created a node struct to hold a set of 6 params.  also typedefed indivudal to the same struct fixed mem leak in tradeSystem fixed insignificant typecast error by returning 1.0 on ERRVAL in tradeSystem changed MAX_NAME_LEN to 7 from 6 to include room for a null char

--- trunk/Src/database.h
+++ trunk/Src/database.h
@@ -1,36 +1,23 @@
-/*
- * header file database.h
- *
- * Database containing the type, tick size and tick value for each commodity
- *
- * Becky Engley and Martin Neal
- * May 2009
- *
- */
+/*!
+  \file    database.h
+  \brief   Global containing the type, tick size, and tick value all commodities
+  \author  Becky Engley and Martin Neal
+  \date    May 2009
+*/
 
 #include "tradeSystem.h"
 
-#define NUMCOMMODS 14
-
-//data is:
-//name
-//type
-//base year
-//number of years (only up to 2004)
-//tick value
-//tick size
-
-commod comDatabase[NUMCOMMODS] = {  {"Jun_CL", LONG, 1983, 21, 10, 0.01},
-                                    {"May_HO", LONG, 1979, 26, 4.2, 0.0001},
-                                    {"Jun_LH", LONG, 1967, 38, 10, 0.025},
-                                    {"Jun_HU", LONG, 1985, 20, 4.2, 0.0001},
-                                    {"Aug_PB", SHORT, 1962, 43, 10, 0.025},
-                                    {"Sep_OJ", SHORT, 1968, 37, 7.5, 0.05},
-                                    {"Dec_C", SHORT, 1949, 56, 12.5, .25},
-                                    {"Dec_LH", SHORT, 1966, 39, 10, 0.025},
-                                    {"Dec_KW", SHORT, 1966, 39, 12.5, 0.25},
-                                    {"Nov_HO", LONG, 1979, 26, 4.2, 0.0001},
-                                    {"Nov_CL", LONG, 1983, 22, 10, 0.01},
-                                    {"Oct_LH", LONG, 1966, 39, 10, 0.025},
-                                    {"Feb_CL", SHORT, 1984, 21, 10, 0.01},
-                                    {"Apr_LC", LONG, 1965, 40, 10, 0.025} };
+commod comDatabase[] = {{"Jun_CL", LONG, 1983, 21, 10, 0.01},
+                        {"May_HO", LONG, 1979, 26, 4.2, 0.0001},
+                        {"Jun_LH", LONG, 1967, 38, 10, 0.025},
+                        {"Jun_HU", LONG, 1985, 20, 4.2, 0.0001},
+                        {"Aug_PB", SHORT, 1962, 43, 10, 0.025},
+                        {"Sep_OJ", SHORT, 1968, 37, 7.5, 0.05},
+                        {"Dec_C", SHORT, 1949, 56, 12.5, .25},
+                        {"Dec_LH", SHORT, 1966, 39, 10, 0.025},
+                        {"Dec_KW", SHORT, 1966, 39, 12.5, 0.25},
+                        {"Nov_HO", LONG, 1979, 26, 4.2, 0.0001},
+                        {"Nov_CL", LONG, 1983, 22, 10, 0.01},
+                        {"Oct_LH", LONG, 1966, 39, 10, 0.025},
+                        {"Feb_CL", SHORT, 1984, 21, 10, 0.01},
+                        {"Apr_LC", LONG, 1965, 40, 10, 0.025}};
