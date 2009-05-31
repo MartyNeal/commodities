d6b2412912b4211ddbd8b396264e2e32a25c9036
Marty.Neal <Marty.Neal@7ba8ebcc-3671-11de-9210-07d028069593>
2009-05-31 18:18:37 +0000

    changed prototypes of learning functions to include extra params used only by ga moved POP_SIZE, ORGY_SIZE, and MUTATE_PROB to parameters. made indPopulation point to heap memory instead of stack memory so as to be dynamic changed learning functions to take in iStepSize instead of iResultSize changed the experiment struct to include additional 3 params for ga.

--- Src/ga.c
+++ Src/ga.c
@@ -10,44 +10,42 @@
 #include <string.h>
 #include "tradeSystem.h"
 
-#define POP_SIZE 30
-#define ORGY_SIZE 6
-#define MUTATE_PROB .05
 
 void Quicksort(individual indArray[], int iSize, int iLeftToSort);
 void Reproduce(individual indParents[], int iParentSize,
                individual indChildren[], int iChildrenSize);
 
 int GeneticAlgorithm(int iIterations, char* szCommodName, int iArgsToRandomize,
-                     int distType, double dResults[], int iResultSize)
+                     int distType, double dResults[], int iStepSize,
+                     int iPopSize, int iOrgySize, double dMutationProb)
 {
     int i,t;
     //create an initial population
-    individual indPopulation[POP_SIZE*2];
-    individual* indChildren = indPopulation+POP_SIZE;
-    int iStepSize;
+    individual* indPopulation;
+    individual* indChildren = indPopulation+iPopSize;
 
-    if (iResultSize == 1)
+    if (iStepSize == 0)
     {
-        FPRINTE(stderr,"iResultSize can't be 1\n");
+        FPRINTE(stderr,"iStepSize can't be 0\n");
         return ERRVAL;
     }
 
-    if (iResultSize > iIterations + 1)
+    if (iStepSize > iIterations)
     {
-        FPRINTE(stderr,"iResultSize can't be greater than iIterations\n");
+        FPRINTE(stderr,"iStepSize can't be greater than iIterations\n");
         return ERRVAL;
     }
 
-    iStepSize = iIterations / (iResultSize - 1);
-    if (iIterations % (iResultSize - 1) != 0)
+    if (iIterations % iStepSize != 0)
     {
-        FPRINTE(stderr,"iResultSize incompatible with iIterations\n");
+        FPRINTE(stderr,"iStepSize incompatible with iIterations\n");
         return ERRVAL;
     }
+
+    indPopulation = (individual*)malloc(iPopSize*2*sizeof(individual));
 
     //fill it with random values
-    for(i = 0; i < POP_SIZE; i++)
+    for(i = 0; i < iPopSize; i++)
     {
         Neighbor(&indPopulation[i].iEntryWindow,
                  &indPopulation[i].iTrailStopWindow,
@@ -68,7 +66,7 @@ int GeneticAlgorithm(int iIterations, char* szCommodName, int iArgsToRandomize,
                             indPopulation[i].szExitDate);
     }
 
-    Quicksort(indPopulation,POP_SIZE,POP_SIZE);
+    Quicksort(indPopulation,iPopSize,iPopSize);
 
     for (t = 0; t < iIterations; ++t)
     {
@@ -86,15 +84,15 @@ int GeneticAlgorithm(int iIterations, char* szCommodName, int iArgsToRandomize,
         }
 
         //reproduce
-        for (i = 0; i < POP_SIZE; i += ORGY_SIZE)
+        for (i = 0; i < iPopSize; i += iOrgySize)
         {
-            Reproduce(indPopulation+i,ORGY_SIZE,indChildren+i,ORGY_SIZE);
+            Reproduce(indPopulation+i,iOrgySize,indChildren+i,iOrgySize);
         }
 
         //mutate
-        for (i = 0; i < POP_SIZE; ++i)
+        for (i = 0; i < iPopSize; ++i)
         {
-            if ((double)random()/RAND_MAX < MUTATE_PROB)
+            if ((double)random()/RAND_MAX < dMutationProb)
             {
                 Neighbor(&indChildren[i].iEntryWindow,
                          &indChildren[i].iTrailStopWindow,
@@ -107,7 +105,7 @@ int GeneticAlgorithm(int iIterations, char* szCommodName, int iArgsToRandomize,
         }
 
         //evaluate children
-        for (i = 0; i < POP_SIZE; ++i)
+        for (i = 0; i < iPopSize; ++i)
         {
             indChildren[i].dProfit =
                 tradeSystemData(szCommodName,
@@ -120,7 +118,7 @@ int GeneticAlgorithm(int iIterations, char* szCommodName, int iArgsToRandomize,
                                 indPopulation[i].szExitDate);
         }
         //cull
-        Quicksort(indPopulation,POP_SIZE*2,POP_SIZE);
+        Quicksort(indPopulation,iPopSize*2,iPopSize);
     }
 
     dResults[t/iStepSize] =
@@ -141,6 +139,8 @@ int GeneticAlgorithm(int iIterations, char* szCommodName, int iArgsToRandomize,
            indPopulation[0].szNoEntryDate,
            indPopulation[0].szExitDate,
            dResults[t/iStepSize]);
+
+    free(indPopulation);
 
     return 0;
 }
